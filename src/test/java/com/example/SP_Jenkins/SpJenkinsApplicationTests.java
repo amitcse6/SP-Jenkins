dcdc916ec6664ebc92e3abb8797570d887152ccd
Amit <amit.mondol@bjitgroup.com>
2024-08-28 14:06:02 +0600

    first commit

---
+++ src/test/java/com/example/SP_Jenkins/SpJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SP_Jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
