185f05740de0e44beecb0464b17c82af92ecf45a
Amit Mondol <amitpstu1@gmail.com>
2024-08-31 10:31:17 +0600

    Test

--- src/main/java/com/example/SP_Jenkins/SpJenkinsApplication.java
+++ src/main/java/com/example/SP_Jenkins/SpJenkinsApplication.java
@@ -8,6 +8,7 @@ public class SpJenkinsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpJenkinsApplication.class, args);
+		System.out.println("Hello World!!");
 	}
 
 }
